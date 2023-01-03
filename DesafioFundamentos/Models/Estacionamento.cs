3ef93f7326d1084bc52de330b1ccb3d6cc356854
Cláudio Silva <claudiojcc1720@gmail.com>
2023-01-03 20:26:00 -0300

    Desafio concluído !

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,16 +16,25 @@ namespace DesafioFundamentos.Models
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
+
+
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.ReadLine();
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
+            Console.WriteLine("Digite a placa do veículo para remover: ");
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            int indexToRemove = 1;
+ 
+            veiculos.RemoveAt(indexToRemove);
+ 
+
+
+            string placa ="";
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -35,8 +44,12 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
+        
                 int horas = 0;
                 decimal valorTotal = 0; 
+                valorTotal = precoInicial + precoPorHora * horas;
+
+
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
@@ -57,6 +70,11 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                for(int contador = 0; contador < veiculos.Count; contador++)
+                {
+                    Console.WriteLine($"Carro N° {contador} - {veiculos[contador]}");
+                }
+                
             }
             else
             {
